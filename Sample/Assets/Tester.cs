5e7b12ac6d945c18f394f9078ed84c14ec7e18e5
harady <harady15@gmail.com>
2013-09-16 02:35:34 +0900

    テストシーン用のスクリプトをC#に変更

---
+++ Sample/Assets/Tester.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tester : MonoBehaviour
+{
+	#region MonoBehaviour Lifecycle Methods
+	void Start ()
+	{
+		AdBannerObserver.Initialize ("a14f12dd9e4d1e0", null, 30.0f);
+	}
+	#endregion
+}
